05543030329a06d9c8ad27d8b2041e2c9f3d37b6
joaubaron <joaubaron@users.noreply.github.com>
2026-06-22 11:00:16 +0000

    deploy: c104471e932c037704fdac42b58bafed5052a08a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '15.06.2026-0951'; // será substituído automaticamente pelo GitHub Actions
+const CACHE_VERSION = '22.06.2026-0800'; // será substituído automaticamente pelo GitHub Actions
 const CACHE_NAME = `meuchef-${CACHE_VERSION}`;
 
 const urlsToCache = [
